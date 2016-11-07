1d88543ad49e94704a93ebbe88039a56c668bb39
Mariana Costa <marianavcc@gmail.com>
2016-11-06 23:26:14 -0200

    Alterações no arquivo

--- excluirAluno.php
+++ excluirAluno.php
@@ -1,15 +1,13 @@
 <?php
 
-	require_once 'conexao.php';
-	
-	echo ("<script type='text/javascript'>option('Deseja mesmo excluir os dados do aluno?')</script>");
+require_once 'conexao.php';
 
-	$matriculaAluno = $_GET['matriculaAluno'];	
+$matriculaAluno = $_GET['matriculaAluno'];
 
+$sql = "DELETE FROM aluno WHERE matriculaAluno = $matriculaAluno";
+$query = mysqli_query($conn, $sql) or die(" Falha ao excluir registro: " . $mysqli_error($conn));
 
-	$sql = "DELETE FROM aluno WHERE matriculaAluno = $matriculaAluno";
-	$query = mysqli_query($conn, $sql) or die(" Falha ao excluir registro:\n " . $mysqli_error($conn));		
+echo "<script type='text/javascript'>alert('Dados do aluno excluídos com sucesso!');location.href='listarAluno.php';</script>";
 
-	echo ("<script type='text/javascript'>alert('Dados do aluno excluídos com sucesso!');location.href='listarAluno.php');</script>");	
-	
-	mysqli_close($conn);
+mysqli_close($conn);
+?>
