2c4998d0abac445239cb4545ed74a72d65727751
Mariana Costa <marianavcc@gmail.com>
2016-11-06 23:28:20 -0200

    Crud tipoUsuario
    Arquivo excluir do crud

---
+++ excluirTipoUsuario.php
@@ -0,0 +1,16 @@
+<?php
+
+require_once 'conexao.php';
+
+$codigoTipoUsuario = $_GET['codigoTipoUsuario'];
+
+$sql = "DELETE FROM tipoUsuario WHERE codigoTipoUsuario = '$codigoTipoUsuario'";
+$query = mysqli_query($conn, $sql) or die("Não foi possível excluir este registro " . mysqli_error($conn));
+
+if ($query) {
+    echo "<script type='text/javascript'>alert('Dados excluídos com sucesso!');location.href='listarTipoUsuario.php';</script>";
+} else {
+    echo "<script type='text/javascript'>alert('Não foi possível excluir este registro');location.href='listaTipoUsuario.php';</script>";
+}
+mysqli_close($conn);
+?>
