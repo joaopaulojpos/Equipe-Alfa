22fff7dcd5a73487be762c4cf944af00d0d905a4
Rhuan Dantas <rhuandantas10@gmail.com>
2016-11-08 17:06:13 -0300

    Inserir turma

---
+++ model/formularioTurma.php
@@ -0,0 +1,24 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Inserir Disciplina</title>
+</head>
+
+<body>
+<form method="POST" action = "cadastroTurma.php">
+	Codigo<br/>
+	<input type = "text" id="codigoturma" name= "codigoturma"/><br/><br/>
+	Ano<br/>
+	<input type = "text" id="ano" name= "ano"/><br/><br/>
+	Turno<br/>
+	<input type = "text" id="turno" name= "turno"/><br/><br/>
+	Periodo<br/>
+	<input type = "text" id="codigoperiodo" name= "codigoperiodo"/><br/><br/>
+	
+	<input type = "submit" value="Inserir"/>
+
+</body>
+
+</html>
