a564398b3520bbf7eed265ffed4b98da812f79c7
renatoredes <renatoredes@live.com>
2016-11-22 11:28:58 -0300

    alterarPeriodo

---
+++ model/alterarPeriodo.php
@@ -0,0 +1,16 @@
+<?php
+require_once 'conexao.php';
+
+if(isset($_POST['codigoPeriodo'])){
+	$codigoPeriodo = $_POST['codigoPeriodo'];
+	$tipoEnsino = $_POST['tipoEnsino'];
+
+	$sql = "UPDATE periodo SET tipoEnsino = '$tipoEnsino' WHERE codigoPeriodo = '$codigoPeriodo'";
+	$query = mysqli_query($conn,$sql) or die("Não foi possível atualizar os dados! " . mysqli_error($conn));	
+
+	echo "<script type='text/javascript'>alert('Dados atualizados com sucesso!');location.href='listarPeriodo.php';</script>";
+
+	mysqli_close($conn);
+	}
+
+?>
